73adae8ac8024cfdb50757eaed700b7e79a55be5
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-24 12:55:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {railroads: {pieces: 4}}
+  monopoly = {railroads: {pieces: 4}, }
 end
 
 def monopoly_with_fourth_tier
