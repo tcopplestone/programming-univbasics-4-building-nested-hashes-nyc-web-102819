8b56f9ab386740f9dfae799cb8dca0d563629923
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-24 13:19:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_third_tier
         shortline_railroad: {},
         b_and_o_railroad: {},
         pennsylvania_railroad: {},
-        
+        reading_railroad: {}
       }
       rent_in_dollars: {
         one_piece_owned: 25,
