89e23ad1fbd232724d54aa0216125178b944288d
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-24 13:24:44 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,55 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  monopoly = {railroads: {}}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly = {railroads: {pieces: 4}, }
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly = {
+    railroads: {
+      pieces: 4,
+      names: {
+        shortline_railroad: {},
+        b_and_o_railroad: {},
+        pennsylvania_railroad: {},
+        reading_railroad: {}
+      }
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
+      }
+    }
+  }
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly = {
+    railroads: {
+      pieces: 4,
+      names: {
+        shortline_railroad: {
+          "mortage_value": "$800"
+        },
+        b_and_o_railroad: {
+          "mortage_value": "$400"
+        },
+        pennsylvania_railroad: {
+          "mortage_value": "$200"
+        },
+        reading_railroad: {
+          "mortage_value": "$100"
+        }
+      }
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
+      }
+    }
+  }
 end
